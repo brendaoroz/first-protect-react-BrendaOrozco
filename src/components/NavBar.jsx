801e44d56c416e9639a124eddbb579d6a261708b
Brenda Orozco <orozcobrenda231@gmail.com>
2024-04-18 21:25:05 -0300

    primer commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from 'style-components'
+
+function NavBar() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default NavBar 
+
+const NavContainer = styles.nav 
