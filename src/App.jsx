41c7b5d6ab872daee702f13582d20ef518f704fa
Brenda Orozco <orozcobrenda231@gmail.com>
2024-04-18 21:33:52 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Navbar from './Navbar';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <Navbar />
+      <div className="container">
+        {}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import styles from 'style-components'
+import React from 'react';
 
-function NavBar() {
-    return (
-        <>
-        </>
-    )
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">Totigian</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar 
-
-const NavContainer = styles.nav 
+export default Navbar;
